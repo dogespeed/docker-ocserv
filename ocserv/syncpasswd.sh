eaed36b866a4f9c75c2eb906e14d3484167950cf
NeverBehave <luoxinhao77@gmail.com>
2017-10-09 14:55:28 +0800

    dynamic API endpoint

--- ocserv/syncpasswd.sh
+++ ocserv/syncpasswd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-URL='https://ssworld.ga/anyconnect.php?key=P3IA6OXBzqPOFfO7MH9NbwwiWtF530VdS8JiJ8HL1Yohhyb2NcO6vmCMfJgW'
+URL='replace this example url'
 wget -O /etc/ocserv/passwd.new $URL > /dev/null
 if [ $? -ne 0 ]; then
 	echo "Failed"
@@ -7,4 +7,3 @@ else
 	echo "Succeeded"
 	mv /etc/ocserv/passwd.new /etc/ocserv/passwd
 fi
-
